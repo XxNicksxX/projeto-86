7878974b6dec5196b51d456bc57c185978d292b6
Nicolas lopes <93435464+XxNicksxX@users.noreply.github.com>
2023-02-22 21:42:53 -0300

    Add files via upload

---
+++ screens/feed.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+} from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import PostCard from "./postCard";
+
+import { FlatList } from "react-native-gesture-handler";
+
+let posts = require("./temp.json");
+
+export default class Feed extends Component {
+  renderItem = ({ item: post }) => {
+    return <PostCard post={post} navigation={this.props.navigation} />;
+  };
+
+  keyExtractor = (item, index) => index.toString();
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <SafeAreaView style={styles.droidSafeArea} />
+        <View style={styles.appTitle}>
+          <View style={styles.appIcon}>
+            <Image
+              source={require("../assets/instagram.png")}
+              style={styles.iconImage}
+            ></Image>
+          </View>
+          <View style={styles.appTitleTextContainer}>
+            <Text style={styles.appTitleText}>instagram</Text>
+          </View>
+        </View>
+        <View style={styles.cardContainer}>
+          <FlatList
+            keyExtractor={this.keyExtractor}
+            data={posts}
+            renderItem={this.renderItem}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "grey",
+  },
+  droidSafeArea: {
+    marginTop:
+      Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35),
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row",
+  },
+  appIcon: {
+    flex: 0.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  iconImage: {
+    width: "200%",
+    height: "200%",
+    resizeMode: "contain",
+  },
+  appTitleTextContainer: {
+    flex: 0.8,
+    justifyContent: "center",
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(25),
+    fontFamily: "Bubblegum-Sans",
+  },
+  cardContainer: {
+    flex: 0.85,
+  },
+});
